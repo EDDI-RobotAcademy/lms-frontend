df1c27106940c97e8984c80ce2d25601f66a8850
moneyandjelly <rmsdldy12@gmail.com>
2024-08-19 11:02:35 +0900

    feat: [이근] 로그인 버튼 추가 [LMSF-18]

--- src/sidebar/CustomSideBar.vue
+++ src/sidebar/CustomSideBar.vue
@@ -6,26 +6,26 @@
         <a id="nav-title" href="https://codepen.io" target="_blank">
           <i class="fab fa-codepen"></i>MENU BOARD
         </a>
-        <label for="nav-toggle">
+        <label for="nav-toggle" @click="navtoogle">
           <span id="nav-toggle-burger"></span>
         </label>
-        <hr>
+        <hr v-if="this.showhrTag">
       </div>
       <div id="nav-content">
         <div class="nav-button"><i class="fas fa-palette"></i><span>New Chat</span></div>
-        <hr>
+        <hr v-if="this.showhrTag">
         <div class="nav-button"><i class="fas fa-images"></i><span>Recents</span></div>
         <div class="nav-button"><i class="fas fa-thumbtack"></i><span>토글 구현 예정</span></div>
         <div class="nav-button"><i class="fas fa-heart"></i><span>구현 예정</span></div>
         <div class="nav-button"><i class="fas fa-chart-line"></i><span>구현 예정</span></div>
         <div class="nav-button"><i class="fas fa-fire"></i><span>구현 예정</span></div>
         <div class="nav-button"><i class="fas fa-magic"></i><span>구현 예정</span></div>
-        <hr>
+        <hr v-if="this.showhrTag">
         <div class="nav-button"><i class="fas fa-gem"></i><span>구현 예정</span></div>
         <div id="nav-content-highlight"></div>
       </div>
       <div id="nav-footer">
-        <div id="nav-footer-heading">
+        <div v-if="isAuthenticated" id="nav-footer-heading">
           <div id="nav-footer-avatar">
             <img src="https://gravatar.com/avatar/4474ca42d303761c2901fa819c4f2547">
           </div>
@@ -34,42 +34,69 @@
             <span id="nav-footer-subtitle">Admin</span>
           </div>
         </div>
+        <button v-if="!isAuthenticated" style="display: block; margin: 0 auto; text-align: center;"
+          id="nav-footer" @click="goToLogin">LOGIN</button>
       </div>
     </nav>
   </div>
 </template>
 
-
-// <script>
-// import { defineComponent } from '@vue/composition-api'
-
-// export default defineComponent({
-//     setup() {
-        
-//     },
-// })
-// </script>
+<script>
+const authenticationModule = "authenticationModule";
+import { mapState } from "vuex";
+import router from "@/router";
+export default ({
+  data() {
+    return {
+      showhrTag: true
+    }
+  },
+  methods: {
+    navtoogle() {
+      if (this.showhrTag) {
+        this.showhrTag = false
+      }
+      else {
+        this.showhrTag = true
+      }
+    },
+    goToLogin(){
+      router.push('/account/login')
+    }
+  },
+  computed: {
+    ...mapState(authenticationModule, ["isAuthenticated"]),
+  },
+});
+</script>
 
 
 <style>
 :root {
-  --background: rgb(255, 194, 111);   /*사이드바 메뉴 hover 색*/
+  --background: rgb(255, 194, 111);
+  /*사이드바 메뉴 hover 색*/
   --navbar-width: 256px;
   --navbar-width-min: 80px;
   /*--navbar-dark-primary: rgba(255, 211, 90,0.8);*/
- --navbar-dark-primary: rgba(227,139,41);    /*사이드바 배경색*/
-  --navbar-dark-secondary: rgb(255, 194, 111);   /*하단 USER, hr선 색*/
-  --navbar-light-primary: #f5f6fa;   /*사이드바 이름(MENU BOARD) 글씨 색*/
-  --navbar-light-secondary: #eff1f3;    /*사이드바 메뉴 글씨 색*/
+  --navbar-dark-primary: rgba(227, 139, 41);
+  /*사이드바 배경색*/
+  --navbar-dark-secondary: rgb(255, 194, 111);
+  /*하단 USER, hr선 색*/
+  --navbar-light-primary: #f5f6fa;
+  /*사이드바 이름(MENU BOARD) 글씨 색*/
+  --navbar-light-secondary: #eff1f3;
+  /*사이드바 메뉴 글씨 색*/
+  --font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
 }
 
-html, body {
+html,
+body {
   margin: 0;
   background: var(--background);
 }
 
 
-#nav-toggle:checked ~ main {
+#nav-toggle:checked~main {
   width: calc(100% - var(--navbar-width-min));
   /* margin-left: var(--navbar-width-min); */
 }
@@ -77,61 +104,61 @@ html, body {
 h1 {
   font-size: 4rem;
   color: #fff;
-  text-shadow: 2px 2px 4px rgba(0,0,0,0.5);
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
   margin-bottom: 2rem;
   text-align: center;
   width: 100%;
 }
 
 /* Navigation bar styles */
-#nav-toggle:checked ~ #nav-header {
+#nav-toggle:checked~#nav-header {
   width: calc(var(--navbar-width-min) - 16px);
 }
 
-#nav-toggle:checked ~ #nav-content,
-#nav-toggle:checked ~ #nav-footer {
+#nav-toggle:checked~#nav-content,
+#nav-toggle:checked~#nav-footer {
   width: var(--navbar-width-min);
 }
 
-#nav-toggle:checked ~ #nav-header #nav-title {
+#nav-toggle:checked~#nav-header #nav-title {
   opacity: 0;
   pointer-events: none;
   transition: opacity .1s;
 }
 
-#nav-toggle:checked ~ #nav-header label[for="nav-toggle"] {
+#nav-toggle:checked~#nav-header label[for="nav-toggle"] {
   left: calc(50% - 8px);
   transform: translate(-50%);
 }
 
-#nav-toggle:checked ~ #nav-header #nav-toggle-burger {
+#nav-toggle:checked~#nav-header #nav-toggle-burger {
   background: var(--navbar-light-primary);
 }
 
-#nav-toggle:checked ~ #nav-header #nav-toggle-burger:before,
-#nav-toggle:checked ~ #nav-header #nav-toggle-burger:after {
+#nav-toggle:checked~#nav-header #nav-toggle-burger:before,
+#nav-toggle:checked~#nav-header #nav-toggle-burger:after {
   width: 16px;
   background: var(--navbar-light-secondary);
   transform: translate(0, 0) rotate(0deg);
 }
 
-#nav-toggle:checked ~ #nav-content .nav-button span {
+#nav-toggle:checked~#nav-content .nav-button span {
   opacity: 0;
   transition: opacity .1s;
 }
 
-#nav-toggle:checked ~ #nav-content .nav-button .fas {
+#nav-toggle:checked~#nav-content .nav-button .fas {
   min-width: calc(100% - 16px);
 }
 
-#nav-toggle:checked ~ #nav-footer #nav-footer-avatar {
+#nav-toggle:checked~#nav-footer #nav-footer-avatar {
   margin-left: 0;
   left: 50%;
   transform: translate(-50%);
 }
 
-#nav-toggle:checked ~ #nav-footer #nav-footer-titlebox,
-#nav-toggle:checked ~ #nav-footer label[for="nav-footer-toggle"] {
+#nav-toggle:checked~#nav-footer #nav-footer-titlebox,
+#nav-toggle:checked~#nav-footer label[for="nav-footer-toggle"] {
   opacity: 0;
   transition: opacity .1s;
   pointer-events: none;
@@ -141,7 +168,7 @@ h1 {
   position: fixed;
   left: 1vw;
   top: 1vw;
-  height: calc(100% - 2vw);
+  height: 90%;
   background: var(--navbar-dark-primary);
   border-radius: 16px;
   display: flex;
@@ -150,14 +177,13 @@ h1 {
   font-family: Verdana, Geneva, Tahoma, sans-serif;
   overflow: hidden;
   user-select: none;
-  z-index: 3; /*APP.vue를 통해 각 페이지마다 연결했을 때 사이드바가 희미하게 보이면서 정상작동을 하지 않을때 z-index의 숫자를 높여야했음.*/
+  z-index: 3;
+  /*APP.vue를 통해 각 페이지마다 연결했을 때 사이드바가 희미하게 보이면서 정상작동을 하지 않을때 z-index의 숫자를 높여야했음.*/
 }
 
 #nav-bar hr {
   margin: 0;
-  position: relative;
-  left: 16px;
-  width: calc(100% - 32px);
+  width: 100vw;
   border: none;
   border-top: solid 1px var(--navbar-dark-secondary);
 }
@@ -187,6 +213,7 @@ h1 {
 
 #nav-header hr {
   position: absolute;
+  right: 0px;
   bottom: 0;
 }
 
@@ -321,14 +348,37 @@ label[for="nav-toggle"] {
   color: var(--navbar-dark-primary);
 }
 
-.nav-button:nth-of-type(1):hover ~ #nav-content-highlight { top: 16px; }
-.nav-button:nth-of-type(2):hover ~ #nav-content-highlight { top: 70px; }
-.nav-button:nth-of-type(3):hover ~ #nav-content-highlight { top: 124px; }
-.nav-button:nth-of-type(4):hover ~ #nav-content-highlight { top: 178px; }
-.nav-button:nth-of-type(5):hover ~ #nav-content-highlight { top: 232px; }
-.nav-button:nth-of-type(6):hover ~ #nav-content-highlight { top: 286px; }
-.nav-button:nth-of-type(7):hover ~ #nav-content-highlight { top: 340px; }
-.nav-button:nth-of-type(8):hover ~ #nav-content-highlight { top: 394px; }
+.nav-button:nth-of-type(1):hover~#nav-content-highlight {
+  top: 16px;
+}
+
+.nav-button:nth-of-type(2):hover~#nav-content-highlight {
+  top: 70px;
+}
+
+.nav-button:nth-of-type(3):hover~#nav-content-highlight {
+  top: 124px;
+}
+
+.nav-button:nth-of-type(4):hover~#nav-content-highlight {
+  top: 178px;
+}
+
+.nav-button:nth-of-type(5):hover~#nav-content-highlight {
+  top: 232px;
+}
+
+.nav-button:nth-of-type(6):hover~#nav-content-highlight {
+  top: 286px;
+}
+
+.nav-button:nth-of-type(7):hover~#nav-content-highlight {
+  top: 340px;
+}
+
+.nav-button:nth-of-type(8):hover~#nav-content-highlight {
+  top: 394px;
+}
 
 #nav-bar .fas {
   min-width: 3rem;
