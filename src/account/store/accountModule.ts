063cbfe3872064b9c406de28b2791a63f434c9f2
moneyandjelly <rmsdldy12@gmail.com>
2024-08-26 10:31:07 +0900

    feat: [이근] 프로필 선택 팝업 추가 [LMSF-36]

--- src/account/store/accountModule.ts
+++ src/account/store/accountModule.ts
@@ -1,5 +1,4 @@
 import actions, { AccountActions } from "./actions"
-import mutations, { AccountMutations } from "./mutations"
 import state, { AccountState } from "./states"
 
 
@@ -7,14 +6,12 @@ export interface AccountModule {
     namespaced: true
     state: AccountState
     actions: AccountActions
-    mutations: AccountMutations
 }
 
 const accountModule: AccountModule = {
     namespaced: true,
     state,
     actions,
-    mutations,
 }
 
 export default accountModule
--- src/account/store/states.ts
+++ src/account/store/states.ts
@@ -1,11 +1,9 @@
 export interface AccountState {
     isLoggedIn: boolean
-    isProfileImg: number
 }
 
 const state: AccountState = {
     isLoggedIn: false,
-    isProfileImg: 0
 }
 
 export default state
