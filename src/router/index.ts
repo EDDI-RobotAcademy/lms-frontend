9abdd01d48740434d658bc07a530a8b8ff5b807c
moneyandjelly <rmsdldy12@gmail.com>
2024-08-30 10:02:36 +0900

    feat: [이근] 사용자가 카카오 페이로 체리를 구매 할 수 있습니다. [LMSF-33]

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import AccountRoutes from '@/account/router/AccountRoutes'
 import ContactRoutes from '@/contact/router/ContactRoutes'
 import aiCommandModule from '@/gatherEverything/store/aiCommandModule'
 import GatherEverythingRoutes from '@/gatherEverything/router/GatherEverythingRoutes'
+import AuthenticationRoutes from '@/authentication/router/AuthenticationRoutes'
 
 const routes: Array<RouteRecordRaw> = [
   ...HomeRoutes,
@@ -14,6 +15,7 @@ const routes: Array<RouteRecordRaw> = [
   ...AccountRoutes,
   ...ContactRoutes,
   ...GatherEverythingRoutes,
+  ...AuthenticationRoutes,
 ]
 
 const router = createRouter({
